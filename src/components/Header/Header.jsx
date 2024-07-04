244e3824961af64fa76d209b3ce54f8a1061a99b
Dmitriy <dmitrian_anatolevich@mail.ru>
2024-07-04 12:52:20 +0200

    csss

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,8 +6,8 @@ export const Header = () => {
 const {onClose, user} = useTelegram()
   return (
     <div className={"header"}>
-      <Button onClick={onClose}>Закрыть</Button>
       <span className={"username"}>{user?.username}</span>
+      <Button onClick={onClose}>Закрыть</Button>
     </div>
   );
 };
