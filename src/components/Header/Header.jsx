30b83b8a4ebcce8764bdee0ce42081d5c841f912
Dmitriy <dmitrian_anatolevich@mail.ru>
2024-07-04 12:10:46 +0200

    added header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import { Button } from "../Button/Button";
+import "./Header";
+
+export const Header = (props) => {
+  const tg = window.Telegram.WebApp;
+  const onClose = () => tg.close();
+
+  return (
+    <div className={"header"}>
+      <Button onClick={onClose}>Закрыть</Button>
+      <span className={"username"}>{tg.initDataUnsafe?.user?.username}</span>
+    </div>
+  );
+};
